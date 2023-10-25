74b2bab987a4e0ed63a960cfa8eed96cc295c454
Indra Kurniawan <kurniawanindra9d@gmail.com>
2023-10-25 19:12:07 +0700

    Add Route

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import './App.css';
 import HomePage from "./pages/HomePage";
+import DetailDestination from "./pages/DetailDestination";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 
 function App() {
   return (
     <>
-      <HomePage />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/detail-destination" element={<DetailDestination />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
